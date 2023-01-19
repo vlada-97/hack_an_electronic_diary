9aee4ba4933332b9824765c6f5467a5671ee3f59
vlada-97 <108741329+vlada-97@users.noreply.github.com>
2023-01-19 11:33:38 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,44 +1,45 @@
 import random
-from django.core.exceptions import ObjectDoesNotExist
 
 from datacenter.models import Schoolkid
 from datacenter.models import Mark
 from datacenter.models import Chastisement
 from datacenter.models import Lesson
-from datacenter.models import Subject
 from datacenter.models import Commendation
-from datacenter.models import Teacher
+
+COMMENDATIONS = [
+    'Хвалю!',
+    'Отлично!',
+    'Молодец!',
+    'Так держать!',
+    'Гораздо лучше, чем я ожидал!',
+    'Ты меня приятно удивил!',
+    'Именно этого я давно ждал от тебя!',
+    'Ты меня очень обрадовал!',
+    'Уже существенно лучше!',
+    'Я вижу, как ты стараешься!',
+    'С каждым разом у тебя получается всё лучше!',
+    'Ты сегодня прыгнул выше головы!'
+    ]
+
+def get_schoolkid(schoolkid_name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        return schoolkid
+    except Schoolkid.DoesNotExist:
+        print('Schoolkid matching query does not exist!')
+    except Schoolkid.MultipleObjectsReturned:
+        print('Return more than one Schoolkid!')
 
 
-def fix_marks(schoolkid_):
-    ocenki = Mark.objects.filter(schoolkid=schoolkid_, points__in=[2,3])
-    for i in ocenki:
-        i.points=5
-        i.save()
+def fix_marks(schoolkid):
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3]).update(points=5)
 
 
-def remove_chastisements(schoolkid_):
-    comment = Chastisement.objects.filter(schoolkid=schoolkid_)
-    for i in comment:
-        i.delete()
+def remove_chastisements(schoolkid):
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
-def create_commendation(schoolkid, subject):
-    comments =  [
-        'Хвалю!',
-        'Отлично!',
-        'Молодец!',
-        'Так держать!',
-        'Гораздо лучше, чем я ожидал!',
-        'Ты меня приятно удивил!',
-        'Именно этого я давно ждал от тебя!',
-        'Ты меня очень обрадовал!',
-        'Уже существенно лучше!',
-        'Я вижу, как ты стараешься!',
-        'С каждым разом у тебя получается всё лучше!',
-        'Ты сегодня прыгнул выше головы!'
-        ]
-    this_schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid).get()
-    lesson = Lesson.objects.filter(year_of_study = this_schoolkid.year_of_study, group_letter=this_schoolkid.group_letter, subject__title__contains=subject).order_by('date')
-    lesson_one = lesson.first()
-    Commendation.objects.create(text=random.choice(comments),schoolkid= this_schoolkid, created= lesson_one.date, teacher=lesson_one.teacher, subject=lesson_one.subject)
+def create_commendation(schoolkid, subject_title):
+    lesson = Lesson.objects.filter(year_of_study = schoolkid.year_of_study, group_letter=schoolkid.group_letter, subject__title__contains=subject_title).order_by('-date').first()
+    Commendation.objects.create(text=random.choice(COMMENDATIONS),schoolkid= schoolkid, created= lesson.date, teacher=lesson.teacher, subject=lesson.subject)
+   
